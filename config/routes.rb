6854c7baf6f843d05ab6cb064005f2d5fe629439
Ariel Pérez <arielperezch@gmail.com>
2015-03-20 12:44:46 -0400

    removed some left overs from the Master application, set up a home controller with an index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Master::Application.routes.draw do
-  resources :unigrams
-
-  resources :mentions
-
-  resources :tickers
 
   get "home/index"
-  get "home/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tickers#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
