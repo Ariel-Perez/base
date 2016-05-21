4ebf2da639a745d02fdaef973f2ecc5b657d1c26
Ariel Pérez <arielperezch@gmail.com>
2016-05-21 14:15:55 -0400

    Trying with static assets.

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Master
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
